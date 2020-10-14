7f522d72dd60e33f1d51f78861f7a5b131b70827
Steveatron3000 <j.r.franklin@aol.com>
2020-10-14 12:56:56 -0500

    Done

---
+++ Exercise3.js
@@ -0,0 +1,23 @@
+var warmHugs = "Hi, I'm Olaf and I like warm hugs!";
+console.log(warmHugs.toUpperCase());
+
+console.log(warmHugs.replace("like", "love"));
+
+//Impaled
+var beenImpaled = "Oh, look at that, I've been impaled.";
+console.log(beenImpaled.slice(18));
+
+//Skull, bones, ...
+var dotDotDot = "...";
+var skullBones = `I don't have a skull ${dotDotDot} or bones`;
+
+console.log(skullBones);
+
+//PI
+console.log(Math.PI);
+
+var randomNumber = Math.random();
+Math.floor(Math.random() * 4); 
+console.log(randomNumber);
+
+
